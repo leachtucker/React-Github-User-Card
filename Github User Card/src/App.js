40258f5e8222999a0f0eb224fd0b42f4fa873b01
Tucker Leach <leachtucker@gmail.com>
2020-09-29 10:12:55 -0700

    Created UserCard component

--- Github User Card/src/App.js
+++ Github User Card/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import UserCard from './Components/UserCard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: {}
+    }
+  }
+
+  // Works with out conditionals in the render function because we change state after the initial rendering
+  componentDidMount() {
+    axios.get('https://api.github.com/users/leachtucker')
+      .then(resp => {
+        this.setState({
+          ...this.state,
+          userData: resp.data
+        });
+      })
+      .catch(err =>{
+        console.log(err)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <UserCard userData={this.state.userData}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ Github User Card/src/Components/UserCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './UserCard.css';
+
+class UserCard extends React.Component {
+    render() {
+        return (
+            <div className="usercard">
+                <img className='avatar-img' src={this.props.userData.avatar_url} alt='Avatar' />
+                <div>
+                    <h3>{this.props.userData.name}</h3>
+                </div>
+                <div className='follow-container'>
+                    <span>Followers: <b>{this.props.userData.followers}</b></span>
+                    <span> </span>
+                    <span>Followers: <b>{this.props.userData.following}</b></span>
+                </div>
+                <div className='repo-container'>
+                    <span>Repos: <b>{this.props.userData.public_repos}</b></span>
+                </div>
+                <div className='bio-container'>
+                    <span>{this.props.userData.bio}</span>
+                </div>
+                <div>
+                    <img src={`http://ghchart.rshah.org/${this.props.userData.login}`} alt="2016rshah's Github chart" />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default UserCard;
